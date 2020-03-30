4d2a9f8aecdc0c40a0f29e87544f2be6a356dd1b
orko3012 <or.ko@hotmail.fr>
2020-03-30 15:17:04 +0200

    react 3 ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Li Yun" avatar="https://randomuser.me/api/portraits/women/70.jpg" online />
-      <Contact name="Simon Raj" avatar="https://randomuser.me/api/portraits/men/28.jpg" />
-      <Contact name="Marie Stu" avatar="https://randomuser.me/api/portraits/women/56.jpg" online />
+      <ContactList />
+      <Contact
+        name="Li Yun"
+        avatar="https://randomuser.me/api/portraits/women/70.jpg"
+        online
+      />
+      <Contact
+        name="Simon Raj"
+        avatar="https://randomuser.me/api/portraits/men/28.jpg"
+      />
+      <Contact
+        name="Marie Stu"
+        avatar="https://randomuser.me/api/portraits/women/56.jpg"
+        online
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import "./Contact.css";
 
-const Contact = (props) => {
+const Contact = props => {
   return (
     <div className="Contact">
-      <img
-        className="avatar"
-        src={props.avatar}
-        alt={props.name}
-      />
+      <img className="avatar" src={props.avatar} alt={props.name} />
       <div>
         <h4 className="name">{props.name}</h4>
         <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"}></div>
+          <div
+            className={props.online ? "status-online" : "status-offline"}
+          ></div>
           <h4 className="status-text">{props.online ? "online" : "offline"}</h4>
         </div>
       </div>
@@ -25,6 +23,6 @@ Contact.propTypes = {
   name: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   online: PropTypes.bool.isRequired
-}
+};
 
 export default Contact;
